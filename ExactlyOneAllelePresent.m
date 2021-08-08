7b67944bd4667e0eaf011456fbca47594ed730f2
greg-harhay <gregory.harhay@usda.gov>
2021-08-08 14:42:33 -0500

    Computes when on a single allele present in case,  control, or both animals in pair

---
+++ ExactlyOneAllelePresent.m
@@ -0,0 +1,35 @@
+% membership for single allele diplotype - returns 1 if single allele
+% present in diplotype
+
+function y = ExactlyOneAllelePresent(allele,diplotype)
+
+% allele either A, C, G, or T
+% diplotype is
+%       R = A,G or G,A
+%       Y = C,T or T,C
+%       S = C,G or G,C
+%       W = A,T or T,A
+%       K = G,T or T,G
+%       M = A,C or C,A
+
+if (allele =='A' && ismember(diplotype, ['R','W','M']))
+    y= 1;
+    return
+elseif (allele == 'C' && ismember(diplotype, ['Y','S','M']))
+    y= 1;
+    return
+elseif (allele == 'G' && ismember(diplotype, ['R','S','K']))
+    y= 1;
+    return
+elseif (allele == 'T' && ismember(diplotype, ['Y','W','K']))
+    y= 1;
+    return
+elseif (allele == 'N' || allele == '0')
+    y = 100; % look for 100 in calling function as signal to throw out pair
+    return
+else
+    y= 0;
+    return
+end
+
+end
